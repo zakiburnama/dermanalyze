3ede3166ef25a1667d33566088dc36413da5cf8a
zakiburnama <zaki.burnama1017@gmail.com>
2022-05-27 06:52:23 +0700

    api maps and change profile

--- app/src/main/java/com/example/dermanalyze_bangkit_capstone/SplashscreenActivity.kt
+++ app/src/main/java/com/example/dermanalyze_bangkit_capstone/SplashscreenActivity.kt
@@ -2,10 +2,12 @@ package com.example.dermanalyze_bangkit_capstone
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowInsets
 import android.view.WindowManager
 
 class SplashscreenActivity : AppCompatActivity() {
@@ -13,26 +15,27 @@ class SplashscreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
+        setupView()
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-//        val loginPreference = LoginPreference(this)
-//        val isLogin = loginPreference.getIsLogin()
-//        val intent: Intent = if (isLogin) {
-//            Intent(this, MainActivity::class.java)
-//        }else {
-//            Intent(this, LoginActivity::class.java)
-//        }
-
-        val intent = Intent(this, IntroActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(intent)
             finish()
         }, delayScreen)
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 
     companion object {
